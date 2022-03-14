ffe08f01e544926b49bd52d8072515e2fe128385
Omnia Salama <34598921+omnia100@users.noreply.github.com>
2022-03-14 23:31:46 +0200

    requirements

--- preprocessing_fuctions.py
+++ preprocessing_fuctions.py
@@ -1,40 +1,11 @@
-
-import codecs
-# import pandas as pd
 import re
 import string
 
-# raw_data = pd.read_json(codecs.open('df.json', 'r', 'utf-8'))
 
-"""# Clean
-
-### mention
-"""
 
 usr = r'(@[A-Za-z0-9]{5,15})\b'
-usr_re = re.compile(usr)
-
-def replace_usr(x):
-  return usr_re.sub('',x)
-
-"""### url"""
-
 url = r'(https?:\/\/?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
-url_re = re.compile(url)
-
-def replace_url(x):
-  return url_re.sub('',x)
-
-"""### hashtag"""
-
 htag = r'(#\w+)'
-htag_re = re.compile(htag)
-
-def replace_htag(x):
-  return htag_re.sub('',x)
-
-"""### emojis"""
-
 emoj = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -56,48 +27,11 @@ emoj = re.compile("["
         u"\u3030"
                       "]+", re.UNICODE)
 
-def remove_emojis(text):
-  return emoj.sub(' ', text)
-
-"""### numbers and english words"""
-
 num_eng =r'[A-Za-z0-9٠-٩]+'
-ne_re = re.compile(num_eng)
-
-def remove_english_and_numbers(text):
-    return re.sub(num_eng, ' ', text)
-
-
-"""### punctuation"""
-
 arabic_punctuations = r'[،\."\(\):\-?!؛…؟]'
-ara_re = re.compile(arabic_punctuations)
-
-def remove_punctuations(text):
-    return re.sub(arabic_punctuations,' ', text)
-
-
-"""### repeated character"""
-
-def remove_repeating_char(text):
-    return re.sub(r'(.)\1+', r'\1', text)
-
-
-"""### marks"""
-
 marks = '''`÷×؛<«»>_()*&^%][ـ،/:"؟.,'{}~¦+|!”…“—–ـ'''
 english_punctuations = string.punctuation
 allMarks = marks + english_punctuations
-
-def remove_marks(text):
-    translator = str.maketrans('', '', allMarks)
-    return text.translate(translator)
-
-
-"""# Normalize
-
-### diacritic
-"""
 
 arabic_diacritics = re.compile("""
                              ّ 
@@ -112,12 +46,45 @@ arabic_diacritics = re.compile("""
                          """, re.VERBOSE)
 
 
+
+
+ne_re = re.compile(num_eng)
+usr_re = re.compile(usr)
+url_re = re.compile(url)
+htag_re = re.compile(htag)
+ara_re = re.compile(arabic_punctuations)
+
+
+def replace_usr(x):
+  return usr_re.sub('',x)
+
+def replace_url(x):
+  return url_re.sub('',x)
+
+def replace_htag(x):
+  return htag_re.sub('',x)
+
+def remove_emojis(text):
+  return emoj.sub(' ', text)
+
+def remove_english_and_numbers(text):
+    return re.sub(num_eng, ' ', text)
+
+def remove_punctuations(text):
+    return re.sub(arabic_punctuations,' ', text)
+
+
+def remove_repeating_char(text):
+    return re.sub(r'(.)\1+', r'\1', text)
+
+
+def remove_marks(text):
+    translator = str.maketrans('', '', allMarks)
+    return text.translate(translator)
+
 def remove_diacritics(text):
     # return [arabic_diacritics.sub('',word) for word in word_list]
     return arabic_diacritics.sub('',text)
-
-
-"""### special characters"""
 
 def normalize_arabic(text):
     text = re.sub("[إأآا]", "ا", text)
@@ -127,9 +94,6 @@ def normalize_arabic(text):
     text = re.sub("ة", "ه", text)
     text = re.sub("گ", "ك", text)
     return text
-
-
-
 
 def preprocess(text):
     text = replace_usr(text)
